397a880460eeabd6b8613955db66cde3a85883eb
Rodrigo Limões <rolimoes@gmail.com>
2022-06-04 20:20:42 -0300

    Ensure class Table return the Markdown Table if a header and data cells is declared.

--- __tests__/unit/generators/table.ts
+++ __tests__/unit/generators/table.ts
@@ -10,4 +10,14 @@ describe("Markdown Table", () => {
       "| NAME | LASTNAME | AGE | \n | :--- | :--- | :--- | \n"
     );
   });
+
+  it("Should be return a header when the header and datacells is expecify", async () => {
+    const table = new Table(data.students, {
+      headers: ["Name", "Age"],
+      dataCells: ["name", "age"],
+    }).generate();
+
+    expect(table).toBeDefined();
+    expect(table).toContain("| Name | Age | \n | :--- | :--- | \n");
+  });
 });
