ec289929d66413b2a55bf293cec4af7fe9d31221
Rodrigo Limões <rolimoes@gmail.com>
2022-07-27 23:45:23 -0300

    Ensure return a unchecked checkbox.

--- __tests__/unit/generators/checkbox.test.ts
+++ __tests__/unit/generators/checkbox.test.ts
@@ -1,10 +1,17 @@
 import CheckBox from "../../../src/libs/generators/checkbox/checkbox";
 
 describe("Checkbox", () => {
-  it("Should return a checkbox", async () => {
+  it("Should return a checked checkbox", async () => {
     const checkbox = new CheckBox(true, "Label 1").generate();
 
     expect(checkbox).toBeDefined();
     expect(checkbox).toEqual("- [X] Label 1 \n");
+  });
+
+  it("Should return a unchecked checkbox", () => {
+    const checkbox = new CheckBox(false, "Label 1").generate();
+
+    expect(checkbox).toBeDefined();
+    expect(checkbox).toEqual("- [ ] Label 1 \n");
   });
 });
