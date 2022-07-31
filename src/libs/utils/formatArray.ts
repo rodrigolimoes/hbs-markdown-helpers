854b95109d844807661b41f2f68fb8d5d03881a2
Rodrigo Limões <rolimoes@gmail.com>
2022-07-31 16:27:52 -0300

    Including utility to format array values.

---
+++ src/libs/utils/formatArray.ts
@@ -0,0 +1,20 @@
+interface FormatArray {
+  elementArray: Array<unknown>;
+  language: string;
+}
+
+export const formatArray = ({ elementArray, language }: FormatArray) => {
+  let array = [...elementArray];
+  let lastValue = array.pop();
+  let connectionString;
+
+  switch (language) {
+    case "pt-br":
+      connectionString = " e ";
+      break;
+    default:
+      connectionString = ", and ";
+      break;
+  }
+  return `${array.join(",")}${connectionString}${lastValue}`;
+};
