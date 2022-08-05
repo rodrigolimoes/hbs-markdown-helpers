6c016fcc3c9e731212c42f2d32a3d79c4399f9fe
Rodrigo Limões <rolimoes@gmail.com>
2022-08-05 09:54:45 -0300

    Adding image markdown generation class

--- src/libs/generators/index.ts
+++ src/libs/generators/index.ts
@@ -2,5 +2,6 @@ import Table from "./table/Table";
 import Checkbox from "./checkbox/Checkbox";
 import CheckList from "./checklist/CheckList";
 import Link from "./link/Link";
+import Image from "./image/Image";
 
-export { Table, Checkbox, CheckList, Link };
+export { Table, Checkbox, CheckList, Link, Image };
