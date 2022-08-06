f4d5daa8fb65e8e721fa06c137ccc26641df3efc
Rodrigo Limões <rolimoes@gmail.com>
2022-08-06 18:44:07 -0300

    Ensure return true when the value is an isoDate

--- __tests__/unit/utils/utilsType.test.ts
+++ __tests__/unit/utils/utilsType.test.ts
@@ -2,11 +2,18 @@ import { isDate } from "../../../src/libs/utils/utilsType";
 
 describe("Utils Type", () => {
   describe("Utils Date", () => {
-    it("Should return a NaN when the value is not an isoDate", async () => {
+    it("Should return false when the value is not an isoDate", async () => {
       const isDateType = isDate("abcd");
 
       expect(isDateType).toBeDefined();
       expect(isDateType).toEqual(false);
+    });
+
+    it("Should return true when the value is an isoDate", () => {
+      const isDateType = isDate("2000-09-01");
+
+      expect(isDateType).toBeDefined();
+      expect(isDateType).toEqual(true);
     });
   });
 });
