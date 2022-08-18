24206bcb026e932b74502387019e5932bcf1d07d
Rodrigo Limões <rolimoes@gmail.com>
2022-08-17 23:22:59 -0300

    Ensure return false when value is a boolean

--- __tests__/unit/utils/utilsType.test.ts
+++ __tests__/unit/utils/utilsType.test.ts
@@ -227,5 +227,12 @@ describe("Utils Type", () => {
       expect(isObjectArrayType).toBeDefined();
       expect(isObjectArrayType).toEqual(false);
     });
+
+    it("Should return false when value is a boolean", () => {
+      const isObjectArrayType = IsObjectArray(true);
+
+      expect(isObjectArrayType).toBeDefined();
+      expect(isObjectArrayType).toEqual(false);
+    });
   });
 });
