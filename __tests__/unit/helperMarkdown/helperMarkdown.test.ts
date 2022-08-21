ed4ea22312f7f57d9dcddc0a693b536533e7dc23
Rodrigo Limões <rolimoes@gmail.com>
2022-08-21 12:16:45 -0300

    Ensure return an image link with url equal void if not specified in params

--- __tests__/unit/helperMarkdown/helperMarkdown.test.ts
+++ __tests__/unit/helperMarkdown/helperMarkdown.test.ts
@@ -312,5 +312,20 @@ describe("HelperMarkdown", () => {
         `[![unknown](../image/image_test.png)](https://github.com/rodrigolimoes/hbs-markdown-helpers/blob/main/__tests__/data/image/image_test.png)`
       );
     });
+
+    it("Should return an image link with url equal void if not specified in params", async () => {
+      const helpers = new HbsMarkdownHelpers({}).getMarkdownHelper();
+      const handlebarsHelper = new HandlebarsHelpers(
+        data.dataOfTableWrong,
+        `{{md_image_link path="../image/image_test.png" description="Table Exemple of hbs-markdown-helpers"}}`
+      );
+      handlebarsHelper.setMarkdownHelper(helpers);
+      const template = await handlebarsHelper.compileTemplate();
+
+      expect(template).toBeDefined();
+      expect(template).toContain(
+        `[![Table Exemple of hbs-markdown-helpers](../image/image_test.png)]()`
+      );
+    });
   });
 });
