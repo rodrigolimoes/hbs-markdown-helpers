d590a27a19f086503e073dc53f1775fab7e4ade7
Rodrigo Limões <rolimoes@gmail.com>
2022-08-20 23:06:55 -0300

    Ensure return false when value is an object

--- __tests__/unit/utils/utilsType.test.ts
+++ __tests__/unit/utils/utilsType.test.ts
@@ -272,5 +272,12 @@ describe("Utils Type", () => {
       expect(isStringType).toBeDefined();
       expect(isStringType).toEqual(false);
     });
+
+    it("Should return false when value is an object", () => {
+      const isStringType = isString({ name: "rodrigo", age: "23" });
+
+      expect(isStringType).toBeDefined();
+      expect(isStringType).toEqual(false);
+    });
   });
 });
